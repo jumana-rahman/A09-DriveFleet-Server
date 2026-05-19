f4af58949acded0984209fc20dbc7ecdf217df0c
jumana-rahman <jumanarahman242@outlook.com>
2026-05-20 02:40:14 +0600

    connected to mongodb

---
+++ index.js
@@ -0,0 +1,70 @@
+const dns = require("dns");
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require("cors");
+dotenv.config();
+const app = express();
+app.use(cors());
+
+const port = process.env.PORT || 8080;
+
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = "mongodb+srv://drivefleet_server:F0wme8QXNBea2e4m@cluster0.wbdlh7s.mongodb.net/?appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    // await client.db("admin").command({ ping: 1 });
+
+    const db = client.db("drivefleetdb");
+    const carsCollection = db.collection("cars");
+
+    app.get("/cars", async(req, res) => {
+        const cursor = carsCollection.find();
+        const result = await cursor.toArray();
+        // console.log(result);
+        res.send(result);
+    });
+
+    app.get("/cars/:id", async(req, res) => {
+        const {id} = req.params;
+        // console.log(id);
+        const query = {_id: new ObjectId(id)};
+        const result = await carsCollection.findOne(query);
+        res.send(result);
+
+    });
+
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+
+// username: drivefleet_server
+// password: F0wme8QXNBea2e4m
