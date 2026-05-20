37939df8431a6c389366ebb6b44876b07b0a8f22
jumana-rahman <jumanarahman242@outlook.com>
2026-05-20 06:36:34 +0600

    env setup for better auth

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 8080;
 
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const uri = "mongodb+srv://drivefleet_server:F0wme8QXNBea2e4m@cluster0.wbdlh7s.mongodb.net/?appName=Cluster0";
+const uri = process.env.MONGODB_URI;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
